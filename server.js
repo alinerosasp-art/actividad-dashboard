7eee667e5b53c1114239f281e2e58f943d9633dc
Aline <aline.rosas.p@gmail.com>
2026-06-12 21:16:00 -0600

    Configuracion para Render

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const app = express();
 
 app.use(express.static(__dirname));
 
-app.listen(3000, () => {
-    console.log('Servidor iniciado en puerto 3000');
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Servidor iniciado en puerto ${PORT}`);
 });
